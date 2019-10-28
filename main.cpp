2826ef4719f0a8de739cff402b4ce665d7263dbf
hj-g18006 <hj-g18006@sist.ac.jp>
2019-10-28 12:27:14 +0900

    最初の変更

--- main.cpp
+++ main.cpp
@@ -1,15 +1,355 @@
+//########## ヘッダーファイル読み込み ##########
 #include "DxLib.h"
 
+//########## マクロ定義 ##########
+#define GAME_WIDTH	800		//画面の横の大きさ
+#define GAME_HEIGHT	600		//画面の縦の大きさ
+#define GAME_COLOR	32		//画面のカラービット
+
+#define GAME_WINDOW_NAME	"GAME TITLE"		//ウィンドウのタイトル
+#define GAME_WINDOW_MODECHANGE	TRUE			//TRUE：ウィンドウモード / FALSE：フルスクリーン
+
+#define SET_WINDOW_ST_MODE_DEFAULT			0	//デフォルト
+#define SET_WINDOW_ST_MODE_TITLE_NONE		1	//タイトルバーなし
+#define SET_WINDOW_ST_MODE_TITLE_FLAME_NONE	2	//タイトルバーとフレームなし
+#define SET_WINDOW_ST_MODE_FLAME_NONE		3	//フレームなし
+
+#define GAME_FPS_SPEED					   60
+
+
+//########## 列挙型 ##########
+enum GAME_SCENE {
+	GAME_SCENE_TITLE,	//タイトル画面
+	GAME_SCENE_PLAY,	//プレイ画面
+	GAME_SCENE_END		//エンド画面
+};
+
+//ウィンドウ関係
+WNDPROC WndProc;						//ウィンドウプロシージャのアドレス
+BOOL IsWM_CREATE = FALSE;				//WM_CREATEが正常に動作したか判断する
+
+//FPS関連
+int StartTimeFps;						//測定開始時刻
+int CountFps;							//カウンタ
+float CalcFps;							//計算結果
+int SampleNumFps = GAME_FPS_SPEED;		//平均を取るサンプル数
+
+//キーボード関連
+char AllKeyState[256];			//すべてのキーの状態が入る
+
+//シーン関連
+int GameSceneNow = (int)GAME_SCENE_TITLE;	//最初のゲーム画面をタイトルに設定
+
+//########## 構造体 ##########
+struct STRUCT_GAZOU {
+	int Handle;				//画像のハンドル
+	char FilePath[128];		//ファイルのパス
+	int X;					//横の位置
+	int Y;					//縦の位置
+	int Width;				//横のサイズ
+	int Height;				//縦のサイズ
+	int C_Width;			//横の中心位置
+	int C_Height;			//縦の中心位置
+
+	int MoveSpeed;			//移動速度
+
+	
+};
+
+
+
+//########## 名前の再定義 ##########
+typedef STRUCT_GAZOU GAZOU;
+
+//########## グローバル変数 ##########
+
+GAZOU BackImage;
+
+//########## プロトタイプ宣言 ##########
+LRESULT CALLBACK MY_WNDPROC(HWND, UINT, WPARAM, LPARAM);	//自作ウィンドウプロシージャ
+
+VOID MY_FPS_UPDATE(VOID);			//FPS値を計測、更新する関数
+//VOID MY_FPS_DRAW(VOID);				//FPS値を描画する関数
+VOID MY_FPS_WAIT(VOID);				//FPS値を計測し、待つ関数
+
+VOID MY_ALL_KEYDOWN_UPDATE(VOID);	//キーの入力状態を更新する関数
+
+VOID MY_GAME_TITLE(VOID);			//タイトル画面の関数
+VOID MY_GAME_PLAY(VOID);			//プレイ画面の関数
+VOID MY_GAME_END(VOID);				//エンド画面の関数
+
+
+
+//########## プログラムで最初に実行される関数 ##########
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	ChangeWindowMode(GAME_WINDOW_MODECHANGE);					//ウィンドウモードに設定
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);			//指定の数値で画面を表示する
+
+	SetWindowStyleMode(SET_WINDOW_ST_MODE_TITLE_NONE);			//タイトルバーなし
+
+	SetMainWindowText(TEXT(GAME_WINDOW_NAME));					//タイトルの文字
+
+	//フック→WM_CLOSEなどのメッセージを引っ掛けて取得する
+	SetHookWinProc(MY_WNDPROC);	//ウィンドウプロシージャの設定
+
 	ChangeWindowMode(TRUE);
-	if (DxLib_Init() == -1)
-		return -1;
+	if (DxLib_Init() == -1) { return -1; }	
+	
+	// 描画先を裏画面に指定
+	if (SetDrawScreen(DX_SCREEN_BACK) != 0) return 0; // 失敗したら強制終了
 
-	DrawBox(0, 0, 100, 100, 0xFFFFFF, TRUE);
+	
 
-	WaitKey();
+	//ＤＸライブラリ初期化処理
 
-	DxLib_End();
+	SetDrawScreen(DX_SCREEN_BACK);								//Draw系関数は裏画面に描画
+
+	//MY_GAZOU_LOAD(&BackImage, 0, 0, GAME_BACKIMAGE);
+
+	//無限ループ
+	while (TRUE)
+	{
+		// プレイヤーの画像を読み込む
+		int imgChara = LoadGraph("media/player_01.png");
+
+		int playerSpeed = 2; // 歩く速さ
+		int playerX = 0; // X座標
+		int playerY = 256; // Y座標
+
+		
+		if (ProcessMessage() != 0) { break; }	//メッセージ処理の結果がエラーのとき、強制終了
+
+		if (ClearDrawScreen() != 0) { break; }	//画面を消去できなかったとき、強制終了
+
+		MY_ALL_KEYDOWN_UPDATE();				//キーの状態を取得
+
+		
+
+		//シーン選択
+		switch (GameSceneNow)
+		{
+		case (int)GAME_SCENE_TITLE:	//タイトル画面の処理ここから
+
+			MY_GAME_TITLE();	//タイトル画面の処理
+
+			break;	//タイトル画面の処理ここまで
+
+		case (int)GAME_SCENE_PLAY:	//プレイ画面の処理ここから
+
+			MY_GAME_PLAY();		//プレイ画面の処理
+
+			break;	//プレイ画面の処理ここまで
+
+		case (int)GAME_SCENE_END:	//エンド画面の処理ここから
+
+			MY_GAME_END();		//エンド画面の処理
+
+			break;	//エンド画面の処理ここまで
+
+		default:
+
+			break;
+
+		}
+
+		MY_FPS_UPDATE();						//FPSの処理[更新]
+		//DrawGraph(BackImage.X, BackImage.Y, BackImage.Handle, TRUE);
+		// 背景の画像を描画
+		
+
+		//MY_FPS_DRAW();		//FPSの処理[描画]
+
+		ScreenFlip();		//モニタのリフレッシュレートの速さで裏画面を再描画
+
+		MY_FPS_WAIT();		//FPSの処理[待つ]
+	}
+	DeleteGraph(BackImage.Handle);
+	//WaitKey();
+
+	DxLib_End();		//ＤＸライブラリ使用の終了処理
+
 	return 0;
+}
+
+//########## FPS値を計測、更新する関数 ##########
+VOID MY_FPS_UPDATE(VOID)
+{
+	if (CountFps == 0) //1フレーム目なら時刻を記憶
+	{
+		StartTimeFps = GetNowCount();
+	}
+
+	if (CountFps == SampleNumFps) //60フレーム目なら平均を計算
+	{
+		int now = GetNowCount();
+		CalcFps = 1000.f / ((now - StartTimeFps) / (float)SampleNumFps);
+		CountFps = 0;
+		StartTimeFps = now;
+	}
+	CountFps++;
+	return;
+}
+
+////########## FPS値を描画する関数 ##########
+//VOID MY_FPS_DRAW(VOID)
+//{
+//	//文字列を描画
+//	DrawFormatString(0, GAME_HEIGHT - 20, GetColor(255, 255, 255), "FPS:%.1f", CalcFps);
+//	return;
+//}
+
+//########## FPS値を計測し、待つ関数 ##########
+VOID MY_FPS_WAIT(VOID)
+{
+	int resultTime = GetNowCount() - StartTimeFps;					//かかった時間
+	int waitTime = CountFps * 1000 / GAME_FPS_SPEED - resultTime;	//待つべき時間
+
+	if (waitTime > 0)		//指定のFPS値よりも早い場合
+	{
+		Sleep(waitTime);	//待つ
+	}
+	return;
+}
+
+//########## キーの入力状態を更新する関数 ##########
+VOID MY_ALL_KEYDOWN_UPDATE(VOID)
+{
+	char TempKey[256];			//一時的に、現在のキーの入力状態を格納する
+
+	GetHitKeyStateAll(TempKey); // 全てのキーの入力状態を得る
+
+	for (int i = 0; i < 256; i++)
+	{
+		if (TempKey[i] != 0)	//押されているキーのキーコードを押しているとき
+		{
+			AllKeyState[i]++;	//押されている
+		}
+		else
+		{
+			AllKeyState[i] = 0;	//押されていない
+		}
+	}
+	return;
+}
+
+//########## タイトル画面の関数 ##########
+VOID MY_GAME_TITLE(VOID)
+{
+	if (AllKeyState[KEY_INPUT_RETURN] != 0)	//エンターキーが押されていた時
+	{
+		GameSceneNow = (int)GAME_SCENE_PLAY;	//シーンをプレイ画面にする
+	}
+
+	//MY_GAZOU_LOAD(&BackGround, 0, 0, GAME_BACKGOUND);
+
+	// 背景の画像を読み込む
+	int imgBack = LoadGraph("image/utyu.jpg");
+
+	DrawGraph(0, 0, imgBack, false);
+
+	
+
+	DrawString(310, 270, "ＧＭＤ", GetColor(255, 255, 255), SetFontSize(60));
+	DrawString(190, 330, "エンターキーを押してください", GetColor(255, 255, 255),SetFontSize(30));
+
+   
+
+	return;
+}
+
+//########## プレイ画面の関数 ##########
+VOID MY_GAME_PLAY(VOID)
+{
+	if (AllKeyState[KEY_INPUT_SPACE] != 0)	//スペースキーが押されていた時
+	{
+		GameSceneNow = (int)GAME_SCENE_END;	//シーンをエンド画面にする
+	}
+
+	int imgBack = LoadGraph("image/utyu2.jpg");
+
+	DrawGraph(0, 0, imgBack, false);
+
+
+	DrawString(0, 0, "プレイ画面(スペースキーを押してください)", GetColor(255, 255, 255));
+
+	return;
+}
+
+//########## エンド画面の関数 ##########
+VOID MY_GAME_END(VOID)
+{
+	if (AllKeyState[KEY_INPUT_BACK] != 0)	//バックスペースーキーが押されていた時
+	{
+		GameSceneNow = (int)GAME_SCENE_TITLE;	//シーンをタイトル画面にする
+	}
+
+	int imgBack = LoadGraph("image/utyu3.jpg");
+
+	DrawGraph(0, 0, imgBack, false);
+
+
+	DrawString(0, 0, "エンド画面(バックスペースキーを押してください)", GetColor(255, 255, 255));
+
+	return;
+}
+
+//########## ウィンドウプロシージャ関数 ##########
+LRESULT CALLBACK MY_WNDPROC(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	switch (msg)
+	{
+
+	case WM_CREATE:	//ウィンドウの生成＆初期化
+
+
+		IsWM_CREATE = TRUE;	//WM_CREATE正常終了
+		return 0;
+
+	case WM_CLOSE:		//閉じるボタンを押したとき
+
+		MessageBox(hwnd, TEXT("ゲームを終了します"), TEXT("終了メッセージ"), MB_OK);
+		break;
+
+	case WM_RBUTTONDOWN:	//マウスの右ボタンを押したとき
+
+		SendMessage(hwnd, WM_CLOSE, 0, 0);		//WM_CLOSEメッセージをキューに追加
+		break;
+
+	case WM_LBUTTONDOWN:	//マウスの左ボタンを押したとき
+
+		//WM_NCLBUTTONDOWN(タイトルバーでマウスの左ボタンを押した)メッセージをすぐに発行
+		PostMessage(hwnd, WM_NCLBUTTONDOWN, (WPARAM)HTCAPTION, lp);
+		break;
+
+	case WM_DESTROY:	//ウィンドウが破棄された(なくなった)とき
+
+		PostQuitMessage(0);		//メッセージキューに WM_QUIT を送る
+		return 0;
+	}
+
+	//デフォルトのウィンドウプロシージャ関数を呼び出す
+	return DefWindowProc(hwnd, msg, wp, lp);
+}
+
+//########## 画像を読み込む設定をする関数 ##########
+//引　数：const char *：読み込む画像のファイルパス
+//引　数：int　：画像の横の位置
+//引　数：int　：画像の縦の位置
+//引　数：画像構造体　：設定する画像構造体の変数
+//戻り値：なし
+VOID MY_GAZOU_LOAD(GAZOU *g, int x, int y, const char *path)
+{
+	wsprintf(g->FilePath, path);						//ファイルパスをコピー
+	g->Handle = LoadGraph(g->FilePath);					//画像をメモリに読み込み、ハンドルを取得
+	GetGraphSize(g->Handle, &g->Width, &g->Height);		//画像サイズを取得
+	g->X = x;
+	g->Y = y;
+	g->C_Width = g->Width / 2;							//画像の横サイズの中心を取得
+	g->C_Height = g->Height / 2;						//画像の縦サイズの中心を取得
+
+	g->MoveSpeed = 10;									//移動速度
+
+	
+	return;
 }
